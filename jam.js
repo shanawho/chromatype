9f7183e5cdb4ab3a45bd71f323e6a0055b0cfb03
shanawho <shanawho@gmail.com>
2013-09-28 12:14:07 -0700

    test commit

--- jam.js
+++ jam.js
@@ -2,8 +2,10 @@
 
 var dict = {};
 
-var color = Math.floor(Math.random()*16777215).toString(16);
+//var color = Math.floor(Math.random()*16777215).toString(16);
 var red = Math.floor((Math.random()*255)+1);
+var blue = Math.floor((Math.random()*255)+1);
+var green = Math.
 
 function changeHeader(String keyword) {
 
