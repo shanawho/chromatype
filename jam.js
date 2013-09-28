d478031ef8d08cb5fdb2eb91ce9b359940029b24
shanawho <shanawho@gmail.com>
2013-09-28 12:08:41 -0700

    hackjam

---
+++ jam.js
@@ -0,0 +1,15 @@
+
+
+var dict = {};
+
+var color = Math.floor(Math.random()*16777215).toString(16);
+var red = Math.floor((Math.random()*255)+1);
+
+function changeHeader(String keyword) {
+
+
+	var elem = document.getElementbyID('header');
+	elem.style.fontFamily= "";
+
+
+}
